vaibhav92/skiboot

commit 6a3cad272e4c494541c2fa5d9b5d893e17adcde3

Add fallback to .version if not git and no SKIBOOT_VERSION
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

make_version.sh
@@ -33,6 +33,11 @@ else
 	then
 		echo $SKIBOOT_VERSION
 	else
-		exit 1;
+		if [ ! -z "`cat .version`" ];
+		then
+			cat .version
+		else
+			exit 1;
+		fi
 	fi
 fi

commit 227251e72221d11d3bf91f06948c15df443ad814

make_version: Fix check for '.version' file
When building from the non-git tree and with '.version' file missing,
make_version.sh shows this bash error message.

% make
...
cat: .version: No such file or directory
...
error You need to set SKIBOOT_VERSION environment variable

This patch fixes this issue by updating '.version' file check in
make_version.sh script to test if it exists as a regular file and is
of non-zero size.

Fixes: 6a3cad272e4c("Add fallback to .version if not git and no SKIBOOT_VERSION")
Signed-off-by: Vaibhav Jain <vaibhav@linux.vnet.ibm.com>

make_version.sh
@@ -50,7 +50,7 @@ else
 	then
 		echo $SKIBOOT_VERSION
 	else
-		if [ ! -z "`cat .version`" ];
+		if [ -f ".version" -a -s ".version" ];
 		then
 			cat .version
 		else